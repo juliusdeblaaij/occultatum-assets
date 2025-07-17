470f3b0a773803167109ab7ece998934a662810b
Julius de Blaaij <julius.de.blaaij@gmail.com>
2025-07-17 22:56:05 +0200

    Initial artifact creation REST api which creates minio files for caching and redirects to a presigned URL for downloading

--- occultatum-python/bases/occultatum/artifact_to_3d_api/main.py
+++ occultatum-python/bases/occultatum/artifact_to_3d_api/main.py
@@ -1,33 +1,49 @@
-from fastapi import FastAPI, HTTPException, Form
-from components.occultatum.artifact_to_3d import artifact_to_3d
-from components.occultatum.simple_object_storage import put_object, get_presigned_url
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from components.occultatum.image_to_glb import image_to_glb
+from components.occultatum.simple_object_storage import put_object, get_presigned_url, get_object
 import httpx
+import datetime
+
+httpx_client = httpx.AsyncClient()
 
 bucket_name = "models"
 
 app = FastAPI()
 
-@app.post("/create_artifact")
+@app.get("/artifact")
 async def create_artifact(
     artifact_id: str,
     image_url: str
 ):
-    # download image from the URL
-    async with httpx.AsyncClient() as client:
-        response = await client.get(image_url)
+    image_file_name = image_url.split("/")[-1]
+
+    image_data = await get_object(bucket_name, f"{artifact_id}/{image_file_name}")
+
+    if not image_data:
+
+        response = await httpx_client.get(image_url)
         if response.status_code != 200:
+            print(f"Failed to download image from {image_url}: {response.status_code} {response.text}")
             raise HTTPException(status_code=400, detail="Failed to download image")
-        file_data = response.content
+        image_data = response.content
 
-    if not (response.headers["Content-Type"] == 'image/jpeg' or response.headers["Content-Type"] == 'image/png'):
-        raise HTTPException(status_code=400, detail=f"{response.headers['Content-Type']} is not an allowed content type. Only JPEG or PNG are allowed.")
+        if not (response.headers["Content-Type"] == 'image/jpeg' or response.headers["Content-Type"] == 'image/png'):
+            raise HTTPException(status_code=400, detail=f"{response.headers['Content-Type']} is not an allowed content type. Only JPEG or PNG are allowed.")
 
-    await put_object(bucket_name, f"{artifact_id}/{image_url.split('/')[-1]}", file_data, content_type=response.headers["Content-Type"])
+        await put_object(bucket_name, f"{artifact_id}/{image_file_name}", image_data, content_type=response.headers["Content-Type"])
 
-    model_data = artifact_to_3d(file_data)
 
-    await put_object(bucket_name, f"{artifact_id}/model.glb", model_data, content_type="model/gltf-binary")
+    model_object_name = f"{artifact_id}/model.glb"
+    model_file = await get_object(bucket_name, model_object_name)
 
-    presigned_url = await get_presigned_url("GET", bucket_name, f"{artifact_id}/model.glb")
+    if not model_file:
+        model_data = image_to_glb(image_data)
+        await put_object(bucket_name, model_object_name, model_data, content_type="model/gltf-binary")
 
-    return {"artifact_id": artifact_id, "presigned_url": presigned_url}
+    # Set expiry to e.g. 3600 seconds (1 hour)
+    expires = datetime.timedelta(seconds=3600)
+    presigned_url = await get_presigned_url("GET", bucket_name, model_object_name, expires=expires)
+
+    response = RedirectResponse(url=presigned_url, status_code=303)
+    return response
